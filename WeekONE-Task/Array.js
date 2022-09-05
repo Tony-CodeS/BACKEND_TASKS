784f7f464963ba3bfa74227ce5f825e33f3063d5
Tony <asolomon484@gmail.com>
2022-09-05 10:48:24 -0700

    WEEK ONE TASK

---
+++ WeekONE-Task/Array.js
@@ -0,0 +1,23 @@
+//“I love algorithms but i think the ones here are too simple”
+//Refer to number 3 above and write a function that takes the entire
+// sentence as an input and returns an array with each of the words in the sentence as items of the array
+
+//4. 
+
+
+const algorithmsSentence = 'I love algorithms but i think the ones here are too simple'
+
+
+const algorithmsArray = [algorithmsSentence]
+
+const algorithmsItems = algorithmsArray.map((items) => items.split(' '))
+
+console.log(algorithmsItems)
+
+
+
+
+
+
+
+  module.exports = {algorithmsItems}
---
+++ WeekONE-Task/Display.js
@@ -0,0 +1,7 @@
+var display = require('./Prime')
+var FizzBuzz = require('./Fizzbuzz')
+var Array = require('./Array')
+
+console.log(display)
+console.log(FizzBuzz)
+console.log(Array)
